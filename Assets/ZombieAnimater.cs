deee8280e7397c3ed5b8c3dd36994d6432563ede
LimeStreem <LimeStreem@gmail.com>
2015-10-02 02:13:44 -0700

    :sparkles: add all of files included in the assignment

---
+++ Assets/ZombieAnimater.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieAnimater : MonoBehaviour {
+
+	public Sprite[] sprites;
+
+	public float framePerSecounds;
+
+	private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<Renderer> () as SpriteRenderer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int index = (int)(Time.timeSinceLevelLoad * framePerSecounds);
+		index = index % sprites.Length;
+		spriteRenderer.sprite = sprites[ index ];
+	}
+}
