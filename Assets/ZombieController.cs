80e482ea48da3547f33f6ed94019109532bd3dfa
LimeStreem <LimeStreem@gmail.com>
2015-10-27 12:15:54 -0700

    :sparkles: finished assignment 4

--- Assets/ZombieController.cs
+++ Assets/ZombieController.cs
@@ -7,6 +7,12 @@ public class ZombieController : MonoBehaviour {
 	public float turnSpeed;
 
 	private Vector3 moveDirection;
+
+
+	[SerializeField]
+	private PolygonCollider2D[] colliders;
+	private int currentColliderIndex = 0;
+
 	// Use this for initialization
 	void Start () {
 		moveDirection = Vector3.right;
@@ -25,6 +31,41 @@ public class ZombieController : MonoBehaviour {
 		transform.position = Vector3.Lerp( currentPosition, target, Time.deltaTime );
 		float targetAngle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
 		transform.rotation = Quaternion.Slerp( transform.rotation, Quaternion.Euler( 0, 0, targetAngle ), turnSpeed * Time.deltaTime );
+		EnforceBounds ();
+	}
 
+	public void SetColliderForSprite( int spriteNum )
+	{
+		colliders[currentColliderIndex].enabled = false;
+		currentColliderIndex = spriteNum;
+		colliders[currentColliderIndex].enabled = true;
+	}
+
+	private void EnforceBounds()
+	{
+		// 1
+		Vector3 newPosition = transform.position; 
+		Camera mainCamera = Camera.main;
+		Vector3 cameraPosition = mainCamera.transform.position;
+		
+		// 2
+		float xDist = mainCamera.aspect * mainCamera.orthographicSize; 
+		float xMax = cameraPosition.x + xDist;
+		float xMin = cameraPosition.x - xDist;
+		
+		// 3
+		if ( newPosition.x < xMin || newPosition.x > xMax ) {
+			newPosition.x = Mathf.Clamp( newPosition.x, xMin, xMax );
+			moveDirection.x = -moveDirection.x;
+		}
+		float yMax = mainCamera.orthographicSize;
+		
+		if (newPosition.y < -yMax || newPosition.y > yMax) {
+			newPosition.y = Mathf.Clamp( newPosition.y, -yMax, yMax );
+			moveDirection.y = -moveDirection.y;
+		}
+		
+		// 4
+		transform.position = newPosition;
 	}
 }
